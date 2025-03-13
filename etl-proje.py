2d5667c82db825893376253d8afbb27ac8559d3a
buğra uysal <uysalbugra134@gmail.com>
2025-03-14 02:08:41 +0300

    The yes and no numbers in the data set are converted to 1 and 0. Data transformation is done, for example price / area.

---
+++ etl-proje.py
@@ -0,0 +1,33 @@
+import pyodbc  
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+aa=pd.read_csv("C:\\Users\\buğra\\Desktop\\Housing.csv")
+
+print(aa.isnull().sum())
+
+print(aa.head())
+
+
+
+print(aa.tail())
+print(aa.info())
+
+
+
+print(aa['parking'])
+print(aa.describe())
+
+aa.replace({"yes":1 ,"no":0},inplace=True)
+print((aa ==0).sum() )
+
+
+aa['price_per_sqft']=aa['price'] /  aa['area']
+
+aa['stories-area']=aa['area'] / aa['stories']
+
+print(aa.head())
