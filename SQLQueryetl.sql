2f01a5b8e217ffc5252479a59cfd7f8b1ca6f896
buğra uysal <uysalbugra134@gmail.com>
2025-03-18 02:12:14 +0300

    Added SQL queries for house price analysis

---
+++ SQLQueryetl.sql
@@ -0,0 +1,34 @@
+select * from Housing
+order by average_room_size desc
+
+select * from Housing
+order by  price_per_sqft desc
+
+select * from Housing
+where lux between 2 and 4
+order by lux desc
+
+select * from housing
+where furnishingstatus='furnished' and bedrooms between 1  and 2 
+
+select * from Housing
+where furnishingstatus='furnished' 
+order by price desc
+
+select * from Housing
+where furnishingstatus='semi-furnished'
+order by price desc
+
+select * from Housing
+where furnishingstatus='unfurnished'
+order by price desc
+
+select * from Housing
+order by parking desc
+
+
+select * from Housing 
+order by lux asc
+
+select * from Housing
+order by parking_bedroom asc
--- etl-proje.py
+++ etl-proje.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 
-aa=pd.read_csv("C:\\Users\\buğra\\Desktop\\Housing.csv")
+aa=pd.read_csv(r"C:/Users\buğra/Desktop/Housing.csv")
 
 print(aa.isnull().sum())
 
@@ -14,22 +14,36 @@ print(aa.head())
 
 
 
-print(aa.tail())
-print(aa.info())
-
-
-
+"""print(aa.tail())
+print(aa.isnull().sum())
 print(aa['parking'])
 print(aa.describe())
+  """
+
+print(aa.info())
 
 aa.replace({"yes":1 ,"no":0},inplace=True)
 print((aa==0).sum())
 
-
+""" Evin fiyatının metrekare başına maliyetini hesaplamak için  """
 aa['price_per_sqft']=aa['price'] / aa['area']
-
+""" Evin kat başına düşen alanı hesaplamak  """
 aa['stories-area']=aa['area'] / aa['stories']
+aa['house-room']=aa[['bedrooms','bathrooms']].sum(axis=1) 
+""" Böylece evlerin donanım seviyesini ölçen bir yeni değişken  """
 aa['lux']= aa[['guestroom', 'hotwaterheating', 'airconditioning', 'prefarea','mainroad','basement']].sum(axis=1)
-aa['prefarea_effect']=aa['prefarea'] * aa['price']
+""" Büyük evlerin yeterince otoparkı var mı? """
 aa['parking_bedroom']= aa['parking'] / aa['bedrooms']
-print(aa.head())
+aa['bedrooms-price']=aa['price'] /aa['bedrooms']
+
+""" ortalama oda büyüklüğü """
+aa['average-room-size']=aa['area']/ aa['house-room']
+""" Fiyatın oda sayısına """
+aa['price-room']=aa['price']/ aa['house-room']
+
+
+
+
+aa.to_csv('C:/Users/buğra/Desktop/Housing.csv', index=False)
+print("csv dosyası indirildi")
+
