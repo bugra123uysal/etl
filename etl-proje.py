e5f6e0f874f62d9ab326cea8f882109cb77b7726
buğra uysal <uysalbugra134@gmail.com>
2025-03-19 01:52:10 +0300

    I added graphics

--- etl-proje.py
+++ etl-proje.py
@@ -18,9 +18,10 @@ print(aa.head())
 print(aa.isnull().sum())
 print(aa['parking'])
 print(aa.describe())
-  """
-
 print(aa.info())
+  """
+ 
+
 
 aa.replace({"yes":1 ,"no":0},inplace=True)
 print((aa==0).sum())
@@ -47,3 +48,46 @@ aa['price-room']=aa['price']/ aa['house-room']
 aa.to_csv('C:/Users/buğra/Desktop/Housing.csv', index=False)
 print("csv dosyası indirildi")
 
+
+
+sns.barplot(y=aa['price'] , hue=aa['furnishingstatus'], data=aa)
+plt.title("sa")
+plt.show()
+
+
+sns.scatterplot(x=aa['price'], y=aa['area'], size=aa['lux'], hue=aa['furnishingstatus' ]  , data=aa)
+plt.show()
+
+sns.countplot(x=aa['bedrooms'], data=aa )
+plt.title('bedrooms count')
+plt.show()
+
+sns.countplot(x=aa['bathrooms'], data=aa)
+plt.title('bathrooms count')
+plt.show()
+
+sns.countplot(x=aa['mainroad'], data=aa)
+plt.title('mainroad count')
+plt.show()
+
+sns.countplot(x=aa['stories'] ,data=aa )
+plt.title('stories count')
+plt.show()
+
+
+sns.countplot(x=aa['house-room'] ,data=aa )
+plt.title('house-room count')
+plt.show()
+
+sns.countplot(x=aa['basement'] ,data=aa )
+plt.title('basement count')
+plt.show()
+
+
+sns.countplot(x=aa['prefarea'] ,data=aa )
+plt.title('prefarea count')
+plt.show()
+
+
+
+print(aa.columns)
